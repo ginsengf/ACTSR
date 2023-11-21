0b4d305ee994316573f91442fbb007aba52950b1
ginsengf <32183337+ginsengf@users.noreply.github.com>
2023-11-21 11:25:20 +0800

    Add files via upload

---
+++ ACTSR/src/demo.sh
@@ -0,0 +1,2 @@
+
+python main.py --model actsr --data_test Set5+Set14+B100+Urban100+Manga109+DIV2K --data_range 801-802 --scale 4 --save actsr_x4 --pre_train X4.pt --rgb_range 1 --test_only --save_results
---
+++ ACTSR/src/main.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+import utility
+import data
+import model
+import loss
+from option import args
+from trainer import Trainer
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES']="7"
+os.environ['CUDA_CACHE_PATH']='~/.cudacache'
+from thop import profile
+# torch.manual_seed(args.seed)
+torch.cuda.empty_cache()
+checkpoint = utility.checkpoint(args)
+
+def main():
+    global model
+    if args.data_test == ['video']:
+        from videotester import VideoTester
+        model = model.Model(args, checkpoint)
+        t = VideoTester(args, model, checkpoint)
+        t.test()
+    else:
+        if checkpoint.ok:
+            loader = data.Data(args)
+            _model = model.Model(args, checkpoint)
+            _loss = loss.Loss(args, checkpoint) if not args.test_only else None
+            t = Trainer(args, loader, _model, _loss, checkpoint)
+            while not t.terminate():
+                t.train()
+                t.test()
+
+            checkpoint.done()
+
+if __name__ == '__main__':
+    main()
